2c6a009074683d73e7108e59229681f93abcf063
Jagdeep Dhaliwal <Jagdeep0000@hotmail.com>
2022-12-08 14:25:55 -0800

    Added base architecture
    Added base code and website URLs for first test item.

--- protein-scraper.py
+++ protein-scraper.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
-from bs4 import bs4
+from bs4 import BeautifulSoup
 import pandas as pd
 
 driver = webdriver.Safari()
 
 products = []
 prices = []
-protein =[]
+proteins =[]
+driver.get("https://www.walmart.ca/en/ip/high-whey/6000202080561")
+
+content = driver.page_source
+soup = BeautifulSoup(content)
+for a in soup.findAll('a', href=True, attrs={'class':'js-content'}):
+    price = a.find('div', attrs={'class':'css-2vqe5n esdkp3p0'})
+    name = a.find('div', attrs={'class':'css-jl2ki2 e1yn5b3f3'})
+    #amount = a.find('div', attrs={'class':''})
+
+    products.append(name.text)
+    prices.append(price.text)
+
+df = pd.DataFrame({'Name':products,'Price':prices})
+df.to_csv('products.csv', index=False, encoding='utf-8')
