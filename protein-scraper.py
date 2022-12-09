c4ade3be9569870971e7217f6beba369d3abfebe
Jagdeep Dhaliwal <Jagdeep0000@hotmail.com>
2022-12-08 16:54:25 -0800

    Recreated Architecture
    Removed redundant dependencies and added wikipedia test website.

--- protein-scraper.py
+++ protein-scraper.py
@@ -1,23 +1,15 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-
-driver = webdriver.Safari()
+from urllib.request import urlopen
 
 products = []
 prices = []
 proteins =[]
-driver.get("https://www.walmart.ca/en/ip/high-whey/6000202080561")
 
-content = driver.page_source
-soup = BeautifulSoup(content)
-for a in soup.findAll('a', href=True, attrs={'class':'js-content'}):
-    price = a.find('div', attrs={'class':'css-2vqe5n esdkp3p0'})
-    name = a.find('div', attrs={'class':'css-jl2ki2 e1yn5b3f3'})
-    #amount = a.find('div', attrs={'class':''})
+url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
 
-    products.append(name.text)
-    prices.append(price.text)
-
-df = pd.DataFrame({'Name':products,'Price':prices})
-df.to_csv('products.csv', index=False, encoding='utf-8')
+print(soup.find("h1", class_ ="css-1c6krh5 e1yn5b3f9"))
+print(soup.find('title').text)
