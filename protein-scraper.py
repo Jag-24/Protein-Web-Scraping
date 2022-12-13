4b53da0e4eb6e5bcf49370f1bd8c7c4de3049f87
Jagdeep Dhaliwal <57514409+Jag-24@users.noreply.github.com>
2022-12-13 10:14:39 -0800

    Added Price data extraction
    Added selenium dependency to facilitate JS loading of page to extract data such as price. Added method for price of product extraction

--- protein-scraper.py
+++ protein-scraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
-import requests
+
 
 products = []
 prices = []
@@ -10,11 +12,13 @@ url = "https://www.walmart.ca/en/ip/high-whey/6000202080561"
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"}
 html = requests.get(url, headers = headers)
 
+browser = webdriver.Firefox()
+browser.get("https://www.walmart.ca/en/ip/high-whey/6000202080561")
+
+soup_test = BeautifulSoup(browser.page_source, 'html.parser')
 soup = BeautifulSoup(html.text, features='html.parser')
 product_name = soup.find('h1', attrs={'data-automation':'product-title'}).text
-product_price = soup.find('span', attrs={'data-automation':'buybox-price'})
-#Test_Desc = soup.find('h1', attrs={'data-automation':'product-title'}).text
+product_price = browser.find_element(By.CSS_SELECTOR, 'span.css-2vqe5n.esdkp3p0').text
 
 print(product_name)
-#print(product_price)
-#print(Test_Desc)
+print(product_price)
