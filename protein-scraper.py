8e5c61ae33a9dcf84c8b37460761f78d7351e57e
Jagdeep Dhaliwal <57514409+Jag-24@users.noreply.github.com>
2022-12-13 12:49:37 -0800

    Updated method
    Removed BeautifulSoup dependency and implemented Selenium method. Created function for extracting product name and price given the url and protein per serving.

--- protein-scraper.py
+++ protein-scraper.py
@@ -2,27 +2,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import requests
+from Product import Product
 
-#Arrays for storing products, prices and protein content 
-products = []
-prices = []
-proteins =[]
+def extract_data(url, protein):
+    browser = webdriver.Firefox()
+    browser.get(url)
+    product_name = browser.find_element(By.TAG_NAME, 'h1').text
+    product_price = browser.find_element(By.CSS_SELECTOR, 'span.css-2vqe5n.esdkp3p0').text
+    return Product(product_name, product_price, protein)
 
-#BeautifulSoup html extraction method
-url = "https://www.walmart.ca/en/ip/high-whey/6000202080561"
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"}
-html = requests.get(url, headers = headers)
 
-#Selenium html and JS extraction method
-browser = webdriver.Firefox()
-browser.get("https://www.walmart.ca/en/ip/high-whey/6000202080561")
 
-#Method of extraction of data using both BeautifulSoup and Selenium
-soup_test = BeautifulSoup(browser.page_source, 'html.parser')
-soup = BeautifulSoup(html.text, features='html.parser')
-product_name = soup.find('h1', attrs={'data-automation':'product-title'}).text
-product_price = browser.find_element(By.CSS_SELECTOR, 'span.css-2vqe5n.esdkp3p0').text
 
-#test prints to verfiy data extraction
-print(product_name)
-print(product_price)
+p = extract_data("https://www.walmart.ca/en/ip/high-whey/6000202080561", 27)
+print(p.name)
+print(p.price)
+print(p.protein)
