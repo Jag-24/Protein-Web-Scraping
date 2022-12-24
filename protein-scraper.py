7d01ba84ed523d816782ad18e13d1420c8e83bb4
Jagdeep Dhaliwal <57514409+Jag-24@users.noreply.github.com>
2022-12-24 11:28:16 -0800

    Added headless browser
    Added the ability to scrape webpage without loading the gui of the firefox browser

--- protein-scraper.py
+++ protein-scraper.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 from Product import Product
 
+options = Options()
+options.add_argument("--headless")
+
 def extract_data(url, protein):
-    browser = webdriver.Firefox()
+    browser = webdriver.Firefox(options = options)
     browser.get(url)
     product_name = browser.find_element(By.TAG_NAME, 'h1').text
     product_price = browser.find_element(By.CSS_SELECTOR, 'span.css-2vqe5n.esdkp3p0').text
