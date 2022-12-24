04ec21c8ed739ad87cb86abee9c08aa55b6cdc4c
Jagdeep Dhaliwal <57514409+Jag-24@users.noreply.github.com>
2022-12-24 12:08:23 -0800

    Added Items
    Added array of URL's to be scraped along with a Protein array to be used in the interim for testing. Added time lib for testing program execution speed.

--- protein-scraper.py
+++ protein-scraper.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from Product import Product
+import time
+
+start_time = time.time()
 
 options = Options()
 options.add_argument("--headless")
@@ -15,9 +18,21 @@ def extract_data(url, protein):
     return Product(product_name, product_price, protein)
 
 
+URLS = ["https://www.walmart.ca/en/ip/high-whey/6000202080561", 
+"https://www.walmart.ca/en/ip/great-value-red-lentils/6000196181072",
+"https://www.walmart.ca/en/ip/iso-whey/6000202081007",
+"https://www.walmart.ca/en/ip/Quaker-Large-Flake-Oats/6000135892801",
+"https://www.walmart.ca/en/ip/Great-Value-Natural-Crunchy-Peanut-Butter/6000198656934",
+"https://www.walmart.ca/en/ip/great-value-long-grain-brown-rice/6000187054949",
+"https://www.walmart.ca/en/ip/mina-halal-boneless-chicken-breasts/6000198434963",
+"https://www.walmart.ca/en/ip/Great-Value-Large-Eggs/6000023483943"]
 
+Protein = [25,9,28,4,4,3,27,6]
 
-p = extract_data("https://www.walmart.ca/en/ip/high-whey/6000202080561", 27)
-print(p.name)
-print(p.price)
-print(p.protein)
+for x,y in zip(URLS,Protein):
+    p = extract_data(x,y)
+    print(p.name)
+    print(p.price)
+    print(p.protein)
+
+print("---%s seconds ---" % (time.time() - start_time))
