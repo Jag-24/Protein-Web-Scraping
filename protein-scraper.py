4426bd290d3b69f0986c23edc44e44f533ae22bb
Jagdeep Dhaliwal <57514409+Jag-24@users.noreply.github.com>
2022-12-25 12:56:23 -0800

    Added logic
    Added logic path to facilitate user interaction and to create more choice paths. Created Data_creation  and user def

--- protein-scraper.py
+++ protein-scraper.py
@@ -17,6 +17,24 @@ def extract_data(url, protein):
     product_price = browser.find_element(By.CSS_SELECTOR, 'span.css-2vqe5n.esdkp3p0').text
     return Product(product_name, product_price, protein)
 
+def user(descision):
+    if descision == "Y":
+        return True
+    elif descision == "N":
+        return False
+    else: 
+        return "Unexpected input"
+
+def Data_creation(URLS, Protein):
+    data = ({"Name": [], "Price": [], "Protein": []})
+    df = pd.DataFrame(data)
+    for x,y in zip(URLS,Protein):
+        print("Currently working on: " + x)
+        p = extract_data(x,y)
+        new_row = pd.DataFrame({"Name":p.name, "Price":p.price, "Protein":p.protein}, index =[0])
+        df = pd.concat([new_row,df.loc[:]]).reset_index(drop=True)
+    return df
+
 
 URLS = ["https://www.walmart.ca/en/ip/high-whey/6000202080561", 
 "https://www.walmart.ca/en/ip/great-value-red-lentils/6000196181072",
@@ -27,16 +45,54 @@ URLS = ["https://www.walmart.ca/en/ip/high-whey/6000202080561",
 "https://www.walmart.ca/en/ip/mina-halal-boneless-chicken-breasts/6000198434963",
 "https://www.walmart.ca/en/ip/Great-Value-Large-Eggs/6000023483943"]
 
+New_URLS =[]
+
 Protein = [25,9,28,4,4,3,27,6]
 
-data = ({"Name": [], "Price": [], "Protein": []})
-df = pd.DataFrame(data)
+New_Protein= []
 
-for x,y in zip(URLS,Protein):
-    print("Currently working on: " + x)
-    p = extract_data(x,y)
-    new_row = pd.DataFrame({"Name":p.name, "Price":p.price, "Protein":p.protein}, index =[0])
-    df = pd.concat([new_row,df.loc[:]]).reset_index(drop=True)
+user_statement = input("Would you like to use a pre-determined list of URL's (Y/N)?")
 
-print(df)
-print("---%s seconds ---" % (time.time() - start_time))
+if user(user_statement) == False:
+    add_or_no = input("Would you like to add to the default list(A) or create a new one(B) (A/B)?")
+    if add_or_no == "A":
+        x = 0
+        adding = int(input("How many URL's would you like to add (integer)?"))
+        while x < adding:
+            url = input("Please enter a URL:")
+            pro = int(input("Please enter the associated protein per serving (integer):"))
+            Protein.append(pro)
+            URLS.append(url)
+            x += 1
+        print("Executing...")
+        print(Data_creation(URLS, Protein))
+    elif add_or_no == "B":
+        x = 0
+        adding = int(input("How many URL's would you like to add (integer)?"))
+        while x < adding:
+            url = input("Please enter a URL:")
+            pro = int(input("Please enter the associated protein per serving (integer):"))
+            New_Protein.append(pro)
+            New_URLS.append(url)
+            x += 1
+        print("Executing...")
+        print(Data_creation(New_URLS, New_Protein))
+    else:
+        print("Unrecognized input please try again later...")
+
+elif user(user_statement) == True:
+    print("Executing...")
+    print(Data_creation(URLS, Protein))
+
+else:
+    print("Unrecogized input please try again later...")
+
+
+#for x,y in zip(URLS,Protein):
+#    print("Currently working on: " + x)
+#    p = extract_data(x,y)
+#    new_row = pd.DataFrame({"Name":p.name, "Price":p.price, "Protein":p.protein}, index =[0])
+#    df = pd.concat([new_row,df.loc[:]]).reset_index(drop=True)
+
+#print(Data_creation)
+#print("---%s seconds ---" % (time.time() - start_time))
